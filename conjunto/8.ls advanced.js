9fe69b04b48da0316eff9294f47e6293ae5cc198
DesaDuana2 <desarrollo2@duanaltda.com>
2025-01-27 14:20:50 -0500

    Se inicia el proyecto de node ini y se instalan dependencias se ajusta tambien para la identacion con asistencia

--- conjunto/8.ls advanced.js
+++ conjunto/8.ls advanced.js
@@ -1,47 +1,50 @@
 // 8.ls advanced.js
 // Sistema de modulos
-const path = require('node:path');
-const fs = require('node:fs/promises');
+const path = require('node:path')
+const fs = require('node:fs/promises')
+const pc = require('picocolors')
 
 // process
-const folder = process.argv[2] ?? '.'; // '.' -> current working directory
+const folder = process.argv[2] ?? '.' // '.' -> current working directory
 
 // Asyncronia secuencial
-async function ls(folder) {
-    let files
+async function ls (folder) {
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch (error) {
+    console.error(pc.red(`No se pudo leer el directorio ${folder} `))
+    process.exit(1)
+  }
+
+  // Asyncronia paralela
+  const filePromises = files.map(async (file) => {
+    const filePath = path.join(folder, file) // Unir rutas
+    let stats
     try {
-        files = await fs.readdir(folder);
+      stats = await fs.stat(filePath) // Devuelve informacion del fichero
     } catch (error) {
-        console.error(`No se pudo leer el directorio ${folder} `);
-        process.exit(1);
+      console.error(
+        pc.red(`No se pudo obtener la informacion del fichero ${filePath}`)
+      )
+      process.exit(1)
     }
 
-    // Asyncronia paralela
-    const filePromises = files.map(async file => {
-        const filePath = path.join(folder, file); // Unir rutas
-        let stats
-        try {
-            stats = await fs.stat(filePath); // Devuelve informacion del fichero
-        } catch (error) {
-            console.error(`No se pudo obtener la informacion del fichero ${filePath}`);
-            process.exit(1);
-        }
+    const isDirectory = stats.isDirectory() // Muestra si es un directorio
+    const fileType = isDirectory ? 'd' : 'f' // Muestra si es un fichero
+    const fileSize = stats.size // Muestra el tamaño del fichero
+    const fileModified = stats.mtime.toLocaleString() // Muestra la fecha de modificacion del fichero
 
-        const isDirectory = stats.isDirectory(); //Muestra si es un directorio
-        const fileType = isDirectory ? 'd' : 'f'; //Muestra si es un fichero
-        const fileSize = stats.size; //Muestra el tamaño del fichero
-        const fileModified = stats.mtime.toLocaleString(); //Muestra la fecha de modificacion del fichero
+    return `${fileType} ${pc.blue(file).padEnd(20)} ${pc.green(
+      fileSize.toString().padStart(10)
+    )} ${pc.yellow(fileModified)}` // Muestra el tipo de fichero, tamaño, fecha de modificacion y nombre
+  })
 
-        return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`; //Muestra el tipo de fichero, tamaño, fecha de modificacion y nombre
-    })
+  const filesInfo = await Promise.all(filePromises)
 
-    const filesInfo = await Promise.all(filePromises);
-
-    filesInfo.forEach(file => {
-        console.log(file);
-    })
+  filesInfo.forEach((file) => {
+    console.log(file)
+  })
 }
 
-ls(folder);
-
-
+ls(folder)
